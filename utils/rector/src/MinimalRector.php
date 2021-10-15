88d58ddccc7927537ef8ffb602aabceb0a8f384c
Matthias Noback <matthiasnoback@gmail.com>
2021-10-15 10:23:52 +0200

    Add an empty rule

---
+++ utils/rector/src/MinimalRector.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Utils\Rector;
+
+use PhpParser\Node;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+final class MinimalRector extends AbstractRector
+{
+    public function getNodeTypes(): array
+    {
+        return [];
+    }
+
+    public function refactor(Node $node): ?Node
+    {
+        return null;
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('A template for a new rule', []);
+    }
+}
