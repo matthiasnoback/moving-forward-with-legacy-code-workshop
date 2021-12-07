a7c5e39ea8f69186d1c8f7d69d0f0c69c501de64
Matthias Noback <matthiasnoback@gmail.com>
2021-12-07 20:22:57 +0100

    Use Laminas RequestHandlerRunner

--- public/index.php
+++ public/index.php
@@ -3,17 +3,24 @@
 declare(strict_types=1);
 
 use App\RequestHandler;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Laminas\Diactoros\ResponseFactory;
 use Laminas\Diactoros\ServerRequestFactory;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use Laminas\HttpHandlerRunner\RequestHandlerRunner;
 use Symfony\Component\ErrorHandler\Debug;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 Debug::enable();
 
-$request = ServerRequestFactory::fromGlobals();
-
-$requestHandler = new RequestHandler();
-$response = $requestHandler->handle($request);
-
-(new SapiEmitter())->emit($response);
+// See https://docs.laminas.dev/laminas-httphandlerrunner/usage/
+$runner = new RequestHandlerRunner(
+    new RequestHandler(),
+    new SapiEmitter(),
+    [ServerRequestFactory::class, 'fromGlobals'],
+    fn (Throwable $throwable) => throw $throwable
+);
+$runner->run();
