a0b3724b0849d49e2b18c75e77b7e23da61ea71f
Matthias Noback <matthiasnoback@gmail.com>
2024-06-10 11:57:26 +0200

    Improve

--- utils/rector/src/Rector/AddServerRequestArgumentRector.php
+++ utils/rector/src/Rector/AddServerRequestArgumentRector.php
@@ -5,18 +5,21 @@ declare(strict_types=1);
 namespace Utils\Rector\Rector;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Param;
+use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
-use PHPStan\Analyser\Scope;
 use PHPStan\Type\ObjectType;
 use Psr\Http\Message\ServerRequestInterface;
-use Rector\Rector\AbstractScopeAwareRector;
+use Rector\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
 /**
  * @see \Utils\Rector\Tests\Rector\AddServerRequestArgumentRector\AddServerRequestArgumentRectorTest
  */
-final class AddServerRequestArgumentRector extends AbstractScopeAwareRector
+final class AddServerRequestArgumentRector extends AbstractRector
 {
     public function getRuleDefinition(): RuleDefinition
     {
@@ -38,30 +41,52 @@ CODE_SAMPLE
      */
     public function getNodeTypes(): array
     {
-        return [ClassMethod::class];
+        return [Class_::class];
     }
 
     /**
-     * @param ClassMethod $node
+     * @param Class_ $node
      */
-    public function refactorWithScope(Node $node, Scope $scope): ?Node
+    public function refactor(Node $node): ?Node
     {
-        if (!$scope->isInClass() || !str_ends_with($scope->getClassReflection()->getName(), 'Controller')) {
+        if (!str_ends_with($this->getName($node) ?: '', 'Controller')) {
             return null;
         }
 
-        foreach ($node->params as $param) {
-            if ($this->isName($param, 'request')) {
-                return null;
+        $modified = false;
+        foreach ($node->stmts as $stmt) {
+            if (! $stmt instanceof ClassMethod) {
+                continue;
             }
-            if ($this->isObjectType($param, new ObjectType(ServerRequestInterface::class))) {
-                return null;
+
+            if ($this->addServerRequestArgumentToControllerMethod($stmt)) {
+                $modified = true;
             }
         }
 
-        $node->params[] = new Node\Param(new Node\Expr\Variable('request'), null,
-            new Node\Name\FullyQualified(ServerRequestInterface::class));
+        return $modified ? $node : null;
+    }
 
-        return $node;
+    private function addServerRequestArgumentToControllerMethod(ClassMethod $classMethod): bool
+    {
+        if (!$classMethod->isPublic()) {
+            return false;
+        }
+        foreach ($classMethod->params as $param) {
+            if ($this->isName($param, 'request')) {
+                return false;
+            }
+            if ($this->isObjectType($param, new ObjectType(ServerRequestInterface::class))) {
+                return false;
+            }
+        }
+
+        $classMethod->params[] = new Param(
+            new Variable('request'),
+            null,
+            new FullyQualified(ServerRequestInterface::class)
+        );
+
+        return true;
     }
 }
--- utils/rector/tests/Rector/AddServerRequestArgumentRector/config/configured_rule.php
+++ utils/rector/tests/Rector/AddServerRequestArgumentRector/config/configured_rule.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 
+use Utils\Rector\Rector\AddServerRequestArgumentRector;
+
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig
         ->importNames();
     $rectorConfig
-        ->rule(\Utils\Rector\Rector\AddServerRequestArgumentRector::class);
+        ->rule(AddServerRequestArgumentRector::class);
 };
