c3b260e55bc2fb320fce892ad71b3b31987563da
Matthias Noback <matthiasnoback@gmail.com>
2024-06-12 13:50:01 +0200

    First rule

---
+++ utils/rector/src/Rector/AddServerRequestArgumentRector.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Utils\Rector\Rector;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Param;
+use Psr\Http\Message\ServerRequestInterface;
+use Rector\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Utils\Rector\Tests\Rector\AddServerRequestArgumentRector\AddServerRequestArgumentRectorTest
+ */
+final class AddServerRequestArgumentRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('// @todo fill the description', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+// @todo fill code before
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+// @todo fill code after
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        // @todo select node type
+        return [\PhpParser\Node\Stmt\ClassMethod::class];
+    }
+
+    /**
+     * @param \PhpParser\Node\Stmt\ClassMethod $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        $node->params[] = new Param(
+            var: new Variable(name: 'request'),
+            type: new FullyQualified(ServerRequestInterface::class)
+        );
+
+        return $node;
+    }
+}
