33569d25b9d8f88a6a4fc62aabb59bb57ca48b1f
Matthias Noback <matthiasnoback@gmail.com>
2024-06-12 10:59:14 +0200

    Use ServerRequestInterface instead of $_GET

--- public/index.php
+++ public/index.php
@@ -3,11 +3,14 @@
 declare(strict_types=1);
 
 use App\Controller\IndexController;
+use Laminas\Diactoros\ServerRequestFactory;
 use Symfony\Component\ErrorHandler\Debug;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 Debug::enable();
 
+$serverRequest = ServerRequestFactory::fromGlobals();
+
 $controller = new IndexController();
-echo $controller->doRun();
+echo $controller->doRun($serverRequest);
